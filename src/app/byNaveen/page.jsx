a60116af69837541c09a021e652d6d828121fab8
Vasu <vasusingh1305@gmail.com>
2024-07-26 22:20:33 +0530

    added naveen page

--- src/app/byNaveen/page.jsx
+++ src/app/byNaveen/page.jsx
@@ -12,12 +12,14 @@ const codingblog=()=>{
                 <div className={styles.letter}>
                         <div className={styles.image}>
 
-                            <Image src="/kkpics/kkandkrati1.jpg" height={500} width={400} className={styles.image}/>
+                            <Image src="/kkpics/kkandnaveen3.jpg" height={500} width={400} className={styles.image}/>
                         </div>
 
                         <div className={styles.text}>
-                            <h1>YES SIR</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis maiores hic veniam vero pariatur voluptatum quam commodi repellat quos, in tempora voluptatibus mollitia iste maxime. Id, quibusdam. Inventore, fugiat alias. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, beatae numquam reiciendis ratione, temporibus in eum maiores, corporis eligendi soluta velit praesentium illum voluptatum quibusdam? Velit facilis eveniet accusamus impedit! </p>
+                            <h1>YO</h1>
+                            <p>
+                                         eeyyy bhaiii ! Kya haal enjoy ur day ham abhi sath hote toh saket tak gedi maar ke aateh or fir gurgaon...miss u ! Happy birthday brother
+                            </p>
                         </div>
 
 
@@ -29,10 +31,12 @@ const codingblog=()=>{
                         <h1>GALLERY</h1>
                     </div>
                     <div className={styles.gallery}>
-                        <Image src="/kkpics/kkandkrati1.jpg" height={300} width={250} className={styles.image}/>
-                        <Image src="/kkpics/kkandkrati1.jpg" height={300} width={250    } className={styles.image}/>
-                        <Image src="/kkpics/kkandkrati3.jpg" height={300} width={250    } className={styles.image}/>
-                        <Image src="/kkpics/kkandkrati4.jpg" height={300} width={250    } className={styles.image}/>
+                        <Image src="/kkpics/kkandnaveen2.jpg" height={300} width={250} className={styles.image}/>
+                        
+                        <Image src="/kkpics/kkandnaveen1.jpg" height={300} width={250    } className={styles.image}/>
+                        <Image src="/kkpics/kkandnaveen5.jpg" height={300} width={250    } className={styles.image}/>
+                        <Image src="/kkpics/kkandnaveen6.jpg" height={300} width={250    } className={styles.image}/>
+                        <Image src="/kkpics/kkandnaveen3.jpg" height={300} width={250    } className={styles.image}/>
                     
 
 
