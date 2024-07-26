6556227634818643eef68b319b90f7b54a781139
Vasu <vasusingh1305@gmail.com>
2024-07-26 21:02:34 +0530

    added gmail

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -1,10 +1,15 @@
+'use client'
 import React from "react";
 import CardList from "../components/CardList/Cardlist";
 import Menu from "../components/Menu/Menu";
 import styles from "./contacts.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { toast } from "react-toastify";
 export const contacts=()=>{
+    const alert = () =>{
+        toast("OMFOO, Naughty hore ho!", {position: 'top-center'})
+    } 
     return(
         <div className={styles.container}>
             <h1 className={styles.title}>Sampark Karein</h1>
@@ -15,8 +20,8 @@ export const contacts=()=>{
                         <Image src="/kkpics/call1.png" width={50} height={50} className={styles.phone}/>
                         <span>+91 78381 01333</span>
                     </Link>
-                    <Link href="mailto:kritagykhanna21k@gmail.com" className={styles.mobile}>
-                        <Image src="/kkpics/gmail.png" width={50} height={50} className={styles.phone}/>
+                    <Link href="mailto:kritagykhanna21k@gmail.com" className={styles.g}>
+                        <Image src="/kkpics/gmaillogo.png" width={100} height={100} className={styles.gmail}/>
                         <span>kritagykhanna21@gmail.com</span>
                         
                     </Link>
@@ -25,11 +30,11 @@ export const contacts=()=>{
                         <Image src="/kkpics/instagramlogo.png" width={500} height={500} className={styles.instagram} alt="/"/>
                         <span>khanna__kritagya</span>
                     </Link>
-                        <Link href="/" className={styles.onlyfans}>
+                        <Link href="" onClick={alert} className={styles.onlyfans}>
                             <Image src="/kkpics/OF1.png" width={500} height={500} className={styles.OF} alt="/"/>
                             <span>KK</span>
                         </Link>
-                    <div className={styles.onlyfans}>fans</div>
+                    
                 </div>
                 
 
